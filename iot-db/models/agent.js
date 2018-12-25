0a8a730e1cb36318dcadf66788407f50e22c6186
Ruben QC <rwquispec@uni.pe>
2018-12-25 00:42:19 -0500

    Desarrollo de modelos de base de datos usando Sequelize

---
+++ iot-db/models/agent.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = function setupAgentModel (config) {
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('agent', {
+    uuid: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    username: {
+      tyoe: Sequelize.STRING,
+      allowNull: false
+    },
+    name: {
+      tyoe: Sequelize.STRING,
+      allowNull: false
+    },
+    hostname: {
+      tyoe: Sequelize.STRING,
+      allowNull: false
+    },
+    pid: {
+      type: Sequelize.INTEGER,
+      allowNull: null
+    },
+    connected: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  })
+}
---
+++ iot-db/models/metric.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = function setupMetricModel (config) {
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('metric', {
+    uuid: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    username: {
+      tyoe: Sequelize.TEXT,
+      allowNull: false
+    }
+  })
+}
