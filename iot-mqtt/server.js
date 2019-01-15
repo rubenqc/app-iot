72aa2bd3d54180033a2c7a0086e87a40db8676a2
Ruben QC <rwquispec@uni.pe>
2019-01-15 12:46:43 -0500

    creado en linux y servidor con mqtt levantado hasta el video 30

--- iot-mqtt/server.js
+++ iot-mqtt/server.js
@@ -18,6 +18,34 @@ const settings = {
 
 const server = new mosca.Server(settings)
 
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Paylaod: ${packet.payload}`)
+})
+
 server.on('ready', () => {
   console.log(`${chalk.green('[iot-mqtt]')} server is running`)
 })
+
+server.on('error', (err) => {
+  debug('Redis connection error', err)
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.log(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.log(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
